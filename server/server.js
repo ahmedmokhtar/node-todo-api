bad4b743cfdf7c86d9a776dcff79bad4d76b25cb
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-10-10 19:00:56 +0200

    Finished n-7-15: Add POST /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,60 +1,26 @@
-const mongoose = require('mongoose')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+const {mongoose} = require('./db/mongoose')
+const {Todo} = require('./models/todo')
+const {User} = require('./models/user')
 
-const Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (err) => {
+        res.status(400).send(err)
+    })
 })
 
-const User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
-})
-
-// const newTodo = new Todo({
-//     text: 'Cook new dinner'
-// })
-
-// newTodo.save().then((doc) => {
-//     console.log('saved todo: ', doc)
-// }, (err) => {
-//     console.log('Unable to save todo', err)
-// })
-
-// const anotherTodo = new Todo({
-//     text: '   Edit this todo    '
-// })
-
-// anotherTodo.save().then((doc) => {
-//     console.log(JSON.stringify(doc, undefined, 2))
-// }, (err) => {
-//     console.log('Unable to save todo', err)
-// })
-
-const newUser = new User({
-    email: '   ahmed@ahmed.com'
-})
-
-newUser.save().then((doc) => {
-    console.log(JSON.stringify(doc, undefined, 2))
-}, (err) => {
-    console.log('Unable to save user', err)
+app.listen(3000, () => {
+    console.log('Server up on port 3000')
 })
