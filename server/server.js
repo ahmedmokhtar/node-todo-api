4e0136904660cbfabe542849d8beb61c73c51ffc
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-10-13 16:35:59 +0200

    Finished n-7-20: added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
@@ -27,6 +28,22 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err)
     })
+})
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({})
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(todo) {
+            res.send({todo})
+        } else {
+            res.status(404).send({})
+        }
+    }).catch((err) => res.status(400).send({}))
 })
 
 app.listen(3000, () => {
