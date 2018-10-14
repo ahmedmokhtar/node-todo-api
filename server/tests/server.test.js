9c87127afd29fa77111cfa2c5980b832cc525bf8
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-10-14 12:38:00 +0200

    Finished n-7-25: Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,3 +96,42 @@ describe('GET /todos:id', () => {
             .expect(404, done)
     })
 })
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        const id = todos[1]._id.toHexString()
+
+        request(app)
+            .delete(`/todos/${id}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(id)
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+                
+                Todo.findById(id).then((todo) => {
+                    expect(todo).toNotExist()
+                    done()
+                }).catch((err) => done(err))
+            })
+       
+    })
+
+    it('should return 404 if todo not found', (done) => {
+        const id = new ObjectID().toHexString()
+
+        request(app)
+            .delete(`/todos/${id}`)
+            .expect(404, done)
+   
+    })
+
+    it('should return 404 for invalid ObjectIDs', (done) => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404, done)
+    })
+})
