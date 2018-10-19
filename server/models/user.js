4da611bb7b1ef9536b5666a2b9f90d0c7909d868
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-10-19 11:08:43 +0200

    Finished n-8-11: added DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,12 @@ UserSchema.methods.generateAuthToken = function () {
     })
 }
 
+UserSchema.methods.removeToken = function (token) {
+    const user = this
+
+    return User.update({$pull: {tokens: {token}}})
+}
+
 UserSchema.statics.findByToken = function (token) {
     const User = this
     let decoded;
